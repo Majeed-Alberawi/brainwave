6fe6b93351b4791c89af1cfadbf34da682cfac2d
Majeed Alberawi <majeedmousa0@gmail.com>
2024-08-25 00:05:13 +0300

    Create Services

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -2,6 +2,7 @@ import { brainwaveSymbol, check } from "../assets";
 import { collabApps, collabContent, collabText } from "../constants";
 import Section from "./Section";
 import Button from "./Button";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
   return (
@@ -72,6 +73,9 @@ const Collaboration = () => {
                 </li>
               ))}
             </ul>
+
+            <LeftCurve />
+            <RightCurve />
           </div>
         </div>
       </div>
